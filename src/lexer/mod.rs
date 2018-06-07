3bdbb8da9c967613fbf36cc847e556f536bbd8a9
Thomas Cheng <thomascheng1998@googlemail.com>
2018-06-07 22:29:25 +0100

    Added lexer bench

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -310,3 +310,26 @@ mod test {
         assert!(tokens.iter().all(|t| t.token_type == super::TokenType::Op));
     }
 }
+
+#[cfg(feature = "bench")]
+mod benches {
+    extern crate test;
+    use self::test::Bencher;
+
+    #[bench]
+    fn test_lex(b: &mut Bencher) {
+        let java_code = r#"
+        package com.tom.test;
+        public class Main {
+            public static void main(String[] args) {
+                float a = 3.f;
+                float b = .2f;
+                float c = a + b;
+                System.out.println("Hello, world!");
+                System.out.println("3 + 0.2 = " + c);
+            }
+        }
+        "#;
+        b.iter(|| test::black_box(super::lex(java_code, "")));
+    }
+}
