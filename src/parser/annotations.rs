b365a938f9aef709d1fe533be6d2ed147cec8a7d
Thomas Cheng <thomascheng1998@googlemail.com>
2018-06-09 18:49:19 +0100

    Added parse_annotations

--- src/parser/annotations.rs
+++ src/parser/annotations.rs
@@ -29,6 +29,19 @@ pub fn parse_annotation(tokens: &mut TokenIter, src: &str) -> ParseRes {
     Ok(nterm(NTermType::Annotation, children))
 }
 
+#[allow(dead_code)]
+pub fn parse_annotations(tokens: &mut TokenIter, src: &str) -> ParseRes {
+    let mut children = vec![parse_annotation(tokens, src)?];
+    while let Some(tok) = tokens.clone().next() {
+        if tok.val(src) == "@" {
+            children.push(parse_annotation(tokens, src)?);
+        } else {
+            break;
+        }
+    }
+    Ok(nterm(NTermType::Annotations, children))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -54,5 +67,13 @@ mod tests {
         assert_eq!(node.children[0].val(src).unwrap(), "@");
         assert_eq!(node.children[2].val(src).unwrap(), "(");
         assert_eq!(node.children[4].val(src).unwrap(), ")");
+    }
+
+    #[test]
+    fn test_parse_annotations() {
+        let src = "@MyAnnotation
+@OtherAnnotation";
+        let node = parse_annotation(&mut lex(src, "").unwrap().iter(), src).unwrap();
+        assert_eq!(node.children.len(), 2);
     }
 }
