7fb4f79be3eb8d2068853ef0529af9d7a704793a
Thomas Cheng <thomascheng1998@googlemail.com>
2018-06-10 12:00:44 +0100

    Parsing par expressions (exprs surrounded with ())

--- src/parser/atoms.rs
+++ src/parser/atoms.rs
@@ -1,4 +1,5 @@
 use super::*;
+use super::expressions::parse_expression;
 use lexer::TokenType;
 
 #[allow(dead_code)]
@@ -14,6 +15,21 @@ pub fn parse_literal(tokens: &mut TokenIter, _src: &str) -> ParseRes {
         Some(tok) => Err(ParseErr::Point("Expected literal".to_owned(), *tok)),
         None => Err(ParseErr::Raw("Expected literal, got EOF".to_owned()))
     }
+}
+
+#[allow(dead_code)]
+pub fn parse_par_expression(tokens: &mut TokenIter, src: &str) -> ParseRes {
+    let mut children = vec![assert_term(tokens, src, "(")?];
+    loop {
+        let tok = tokens.clone().next();
+        match tok {
+            Some(tok) if tok.val(src) == ")" => break,
+            None => return Err(ParseErr::Raw("Unexpected EOF in expression".to_owned())),
+            _ => children.push(parse_expression(tokens, src)?),
+        }
+    }
+    children.push(assert_term(tokens, src, ")")?);
+    Ok(nterm(NTermType::ParExpression, children))
 }
 
 #[allow(dead_code)]
@@ -33,5 +49,12 @@ mod tests {
         assert!(src.iter().all(|src| {
             parse_literal(&mut lex(src, "").unwrap().iter(), src).is_ok()
         }));
+    }
+
+    #[test]
+    fn test_parse_par_expression() {
+        let src = "()";
+        let node = parse_par_expression(&mut lex(src, "").unwrap().iter(), src).unwrap();
+        assert_eq!(node.children.len(), 2);
     }
 }
--- src/parser/expressions.rs
+++ src/parser/expressions.rs
@@ -1,5 +1,9 @@
 use super::*;
 
+pub fn parse_expression(_tokens: &mut TokenIter, _src: &str) -> ParseRes {
+    unimplemented!()
+}
+
 pub fn parse_expression1(_tokens: &mut TokenIter, _src: &str) -> ParseRes {
     unimplemented!()
 }
