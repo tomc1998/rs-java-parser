275230ed2a832b703daf43d136ca5b50508595a5
Thomas Cheng <thomascheng1998@googlemail.com>
2018-06-12 09:31:40 +0100

    Added parsing variable modifiers

--- src/parser/variables.rs
+++ src/parser/variables.rs
@@ -1,8 +1,12 @@
 use super::*;
+use super::annotations::parse_annotation;
 
-#[allow(dead_code)]
-pub fn parse_variable_modifier(_tokens: &mut TokenIter, _src: &str) -> ParseRes {
-    unimplemented!()
+pub fn parse_variable_modifier(tokens: &mut TokenIter, src: &str) -> ParseRes {
+    let child = match tokens.clone().next() {
+        Some(tok) if tok.val(src) == "final" => term(*tokens.next().unwrap()),
+        _ => parse_annotation(tokens, src)?,
+    };
+    Ok(nterm(NTermType::VariableModifier, vec![child]))
 }
 
 #[allow(dead_code)]
@@ -10,3 +14,24 @@ pub fn parse_variable_declarators(_tokens: &mut TokenIter, _src: &str) -> ParseR
     unimplemented!()
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use lexer::lex;
+
+    #[test]
+    fn test_parse_variable_modifier() {
+        let src = "@MyAnnotation";
+        let node = parse_variable_modifier(&mut lex(src, "").unwrap().iter(), src).unwrap();
+        assert_eq!(node.children.len(), 1);
+        match node.children[0].node_type {
+            NodeType::NTerm(NTermType::Annotation) => (),
+            _ => panic!("Wrong nterm type"),
+        }
+
+        let src = "final";
+        let node = parse_variable_modifier(&mut lex(src, "").unwrap().iter(), src).unwrap();
+        assert_eq!(node.children.len(), 1);
+        assert_eq!(node.children[0].val(src), Some("final"));
+    }
+}
