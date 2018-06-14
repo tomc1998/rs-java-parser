2398fc563deb8c5fa40594900f8fdc41facd167c
Thomas Cheng <thomascheng1998@googlemail.com>
2018-06-14 10:41:40 +0100

    Fixed test for parse_catches

--- src/parser/try_catches.rs
+++ src/parser/try_catches.rs
@@ -102,7 +102,10 @@ mod tests {
 
     #[test]
     pub fn test_parse_catches() {
-        let src = "catch (IOException | SocketException e) {}";
+        let src = "catch (IOException | SocketException e) {
+} catch (Exception e) {
+}
+";
         let node = parse_catches(&mut lex(src, "").unwrap().iter(), src);
         let node = node.unwrap();
         assert_eq!(node.children.len(), 2);
