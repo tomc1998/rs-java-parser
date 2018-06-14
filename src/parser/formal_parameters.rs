9ec41ca22224e3f62d100053a9c341f491fef054
Thomas Cheng <thomascheng1998@googlemail.com>
2018-06-14 10:34:08 +0100

    Added parsing for formal parameters

--- src/parser/formal_parameters.rs
+++ src/parser/formal_parameters.rs
@@ -1,7 +1,62 @@
+use lexer::TokenType;
 use super::*;
+use super::types::parse_type;
+use super::variables::parse_variable_modifier;
 
 #[allow(dead_code)]
-pub fn parse_variable_declarator_id(_tokens: &mut TokenIter, _src: &str) -> ParseRes {
-    unimplemented!()
+pub fn parse_formal_parameters(tokens: &mut TokenIter, src: &str) -> ParseRes {
+    let mut children = vec![assert_term(tokens, src, "(")?];
+    match tokens.clone().next() {
+        Some(tok) if tok.val(src) == ")" => (),
+        _ => children.push(parse_formal_parameter_decls(tokens, src)?),
+    }
+    children.push(assert_term(tokens, src, ")")?);
+    Ok(nterm(NTermType::FormalParameters, children))
+}
+
+#[allow(dead_code)]
+pub fn parse_formal_parameter_decls(tokens: &mut TokenIter, src: &str) -> ParseRes {
+    let mut children = Vec::new();
+    while let Some(tok) = tokens.clone().next() {
+        match tok.val(src) {
+            "final" | "@" => children.push(parse_variable_modifier(tokens, src)?),
+            _ => break
+        }
+    }
+    children.push(parse_type(tokens, src)?);
+    children.push(parse_formal_parameter_decls_rest(tokens, src)?);
+    Ok(nterm(NTermType::FormalParameterDecls, children))
+}
+
+#[allow(dead_code)]
+pub fn parse_formal_parameter_decls_rest(tokens: &mut TokenIter, src: &str) -> ParseRes {
+    let children = match tokens.clone().next() {
+        Some(tok) if tok.val(src) == "..." => vec![
+            term(*tokens.next().unwrap()),
+            parse_variable_declarator_id(tokens, src)?],
+        _ => {
+            let mut children = vec![parse_variable_declarator_id(tokens, src)?];
+            while let Some(tok) = tokens.clone().next() {
+                if tok.val(src) == "," {
+                    tokens.next().unwrap(); // Consume ","
+                    children.push(parse_formal_parameter_decls(tokens, src)?);
+                } else { break }
+            }
+            children
+        }
+    };
+    Ok(nterm(NTermType::FormalParameterDeclsRest, children))
+}
+
+#[allow(dead_code)]
+pub fn parse_variable_declarator_id(tokens: &mut TokenIter, src: &str) -> ParseRes {
+    let mut children = vec![assert_term_with_type(tokens, TokenType::Ident)?];
+    while let Some(tok) = tokens.clone().next() {
+        if tok.val(src) == "[" {
+            children.push(term(*tokens.next().unwrap()));
+            children.push(assert_term(tokens, src, "]")?);
+        } else { break }
+    }
+    Ok(nterm(NTermType::VariableDeclaratorId, children))
 }
 
