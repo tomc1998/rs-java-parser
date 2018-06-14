e77cda7128a5052d24e9777c5ca87e75742fb42b
Thomas Cheng <thomascheng1998@googlemail.com>
2018-06-14 07:46:41 +0100

    Added creator parser

--- src/parser/creators.rs
+++ src/parser/creators.rs
@@ -1,4 +1,20 @@
 use super::*;
+use super::types::parse_non_wildcard_type_arguments;
+
+#[allow(dead_code)]
+pub fn parse_created_name(_tokens: &mut TokenIter, _src: &str) -> ParseRes {
+    unimplemented!()
+}
+
+#[allow(dead_code)]
+pub fn parse_array_creator_rest(_tokens: &mut TokenIter, _src: &str) -> ParseRes {
+    unimplemented!()
+}
+
+#[allow(dead_code)]
+pub fn parse_class_creator_rest(_tokens: &mut TokenIter, _src: &str) -> ParseRes {
+    unimplemented!()
+}
 
 #[allow(dead_code)]
 pub fn parse_identifier_suffix(_tokens: &mut TokenIter, _src: &str) -> ParseRes {
@@ -11,6 +27,18 @@ pub fn parse_selector(_tokens: &mut TokenIter, _src: &str) -> ParseRes {
 }
 
 #[allow(dead_code)]
-pub fn parse_creator(_tokens: &mut TokenIter, _src: &str) -> ParseRes {
-    unimplemented!()
+pub fn parse_creator(tokens: &mut TokenIter, src: &str) -> ParseRes {
+    let children = match tokens.clone().next() {
+        Some(tok) if tok.val(src) == "<" => vec![
+            parse_non_wildcard_type_arguments(tokens, src)?,
+            parse_created_name(tokens, src)?,
+            parse_class_creator_rest(tokens, src)?],
+        _ => vec![
+            parse_created_name(tokens, src)?,
+            match tokens.clone().next() {
+                Some(tok) if tok.val(src) == "[" => parse_array_creator_rest(tokens, src)?,
+                _ => parse_class_creator_rest(tokens, src)?,
+            }]
+    };
+    Ok(nterm(NTermType::Creator, children))
 }
